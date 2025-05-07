94c6c64680235855d2b57232ab5d78b5195b84e3
Kamblesuraj432 <140583784+kamblesuraj432@users.noreply.github.com>
2025-05-07 09:26:42 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        <Footer />
+    </Router>
+  );
+}
+
+export default App;
